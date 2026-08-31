b562012841e3318686566e185ef0fa80bb68d10a
21042009-pa <paula.gianotto@aluno.senai.br>
2026-08-31 14:29:29 -0300

    TAILWIND CONFIG

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./js/**/*.js",
+    "./src/**/*.{html,js}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
